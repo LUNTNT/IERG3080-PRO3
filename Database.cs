87915c58ec8fb80ea9a6ec30b2c9965040602f99
LUNTNT <93472532+LUNTNT@users.noreply.github.com>
2021-11-11 16:01:30 +0800

    Create Database.cs

---
+++ Database.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace IERG3080_PRO3
+{
+  
+  public class MongoDB
+  {
+        protected MongoClient client = new MongoClient("localhost:27017");
+        protected MongoClient.GetDatabase db = client.GetDatabase("IERG3080_DB");
+
+  }
+
+}
