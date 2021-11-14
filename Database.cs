81ca3e0e36467fbba3ffa40c1e1f8f5fba5d072a
LUNTNT <93472532+LUNTNT@users.noreply.github.com>
2021-11-14 23:43:51 +0800

    update model

--- Database.cs
+++ Database.cs
@@ -6,14 +6,34 @@ using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Driver;
 
-namespace IERG3080_PRO3
+namespace Database
 {
-  
-  public class MongoDB
-  {
-        protected MongoClient client = new MongoClient("localhost:27017");
-        protected var db = client.GetDatabase("IERG3080_DB");
 
-  }
+    public class MongoDB
+    {
+        private static string connection = "mongodb+srv://admin:<admin>@cluster0.ag5gb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+        protected static MongoClient client = new MongoClient(connection);
+        protected static IMongoDatabase db = client.GetDatabase("3080Proj");
+
+        protected static IMongoCollection<BsonDocument> UserColl = db.GetCollection<BsonDocument>("Users");
+        protected static IMongoCollection<BsonDocument> ProblemColl = db.GetCollection<BsonDocument>("Problems");
+        protected static IMongoCollection<BsonDocument> SubmissionColl = db.GetCollection<BsonDocument>("Submission");
+        public MongoDB()
+        {
+        }
+
+        public virtual void getAll() { }
+  
+    }
+
+    public class UsersCol: MongoDB
+    {
+        public UsersCol()
+        {
+        }
+
+        public override List<Model.Users> getAll() { return}
+    }
 
 }
---
+++ Model.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Users
+    {
+        public string userID;
+        public string password;
+        public string userType;
+
+        public string name;
+    }
+
+    public class Problems
+    {
+        public string ID;
+        public string title;
+        public string content;
+        public string author;
+
+        public int trial;
+        public int difficulty;
+        public int timeLimit;
+        public int memoryLimit;
+
+        public double acRate;
+        public double tags;
+
+        public bool isLive;
+    }
+
+    public class Submissions
+    {
+        public string submissionID;
+        public string userID;
+        public string problemID;
+
+        public string program;
+        public int result;
+    }
+}
