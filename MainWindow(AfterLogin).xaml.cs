d4ab01a2cfed51d2a1e909d9752744161278e199
XXXLokXXX <chunloklok912@gmail.com>
2021-11-16 18:47:34 +0800

    m

---
+++ MainWindow(AfterLogin).xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IERG3080_PRO3
+{
+    /// <summary>
+    /// MainWindow_AfterLogin_.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow_AfterLogin_ : Page
+    {
+        public MainWindow_AfterLogin_()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Problem.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IERG3080_PRO3
+{
+    /// <summary>
+    /// Problem.xaml 的互動邏輯
+    /// </summary>
+    public partial class Problem : Page
+    {
+        public Problem()
+        {
+            InitializeComponent();
+        }
+
+        private void Language_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
