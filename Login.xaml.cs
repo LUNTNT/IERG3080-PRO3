180ea43246dc2cf58af96cbbdae6518bb27ebad4
XXXLokXXX <chunloklok912@gmail.com>
2021-11-16 14:07:31 +0800

    login

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -13,25 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-<<<<<<< HEAD:Login.xaml.cs
-namespace IERG3080_PRO3
-=======
 namespace IERG3080_PRO3_Login
->>>>>>> 2b842a9f5e6886bc49c443f8bc49070e9b109c06:Interface/Login.xaml.cs
 {
-    /// <summary>
-    /// Login.xaml 的互動邏輯
-    /// </summary>
-    public partial class Login : Page
+    public partial class Login : Window
     {
         public Login()
         {
             InitializeComponent();
         }
 
-        internal void Show()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
