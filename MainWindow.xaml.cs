27a72cee904c33e145ddb46ec3704b591499cf63
LUNTNT <93472532+LUNTNT@users.noreply.github.com>
2021-11-18 11:33:32 +0800

    nyry

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -62,7 +62,18 @@ namespace IERG3080_PRO3
             backlogin.Show();
             this.Close();
         }
+
+        private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+            DataGridRow row = sender as DataGridRow;
+            // Some operations with this row
+            Model.Problems selectedProblem = (Model.Problems)row.Item;
+            this.Content = new ProblemPage.ProblemPage(selectedProblem);
+        }
     }
+
+
 
     public class JudgeSystem
     {
